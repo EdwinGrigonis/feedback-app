e1c87b23d20eabdacd695aa180e9395b5107cb25
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-03-25 16:03:08 +0900

    updated deleteFeedback

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData);
+    const [feedback, setFeedback] = useState(FeedbackData)
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete this comment?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
 
     return (
         <>  
             <Header text="Feedback App" />
-            <FeedbackList feedback={feedback} />
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
         </>
     )
 }
