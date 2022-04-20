c09155495559db542f53f99df8d1308badb78339
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-04-20 11:14:29 +0900

    configured add to json server

--- src/components/context/FeedbackContext.jsx
+++ src/components/context/FeedbackContext.jsx
@@ -1,4 +1,3 @@
-import {v4 as uuidv4} from "uuid";
 import { createContext, useState, useEffect } from "react";
 
 const FeedbackContext = createContext()
@@ -18,16 +17,24 @@ export const FeedbackProvider = ({children}) => {
 
     //Fetch feedback
     const fetchFeedback = async () => {
-        const response = await fetch("http://localhost:5000/feedback?_sort=id&order=desc")
+        const response = await fetch("/feedback?_sort=id&order=desc")
         const data = await response.json() 
         setFeedback(data)
         setIsLoading(false)
     }
 
     //Add feedback
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4()
-        setFeedback([newFeedback, ...feedback])
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch("/feedback", {
+            method: 'POST', 
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(newFeedback)
+                
+        })
+
+        const data = await response.json()
+
+        setFeedback([data, ...feedback])
     }
 
     const deleteFeedback = (id) => {
