a105cd2a4a18b570ce18e072e38db4c2a52274a8
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-04-21 10:52:14 +0900

    fixed update

--- src/components/context/FeedbackContext.jsx
+++ src/components/context/FeedbackContext.jsx
@@ -37,15 +37,31 @@ export const FeedbackProvider = ({children}) => {
         setFeedback([data, ...feedback])
     }
 
-    const deleteFeedback = (id) => {
+    //Delete feedback
+
+    const deleteFeedback = async (id) => {
         if(window.confirm('Are you sure you want to delete this comment?')) {
+            await fetch(`/feedback/${id}`, {method: 'DELETE'})
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
-    const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => item.id === id? {
-            ...item, ...updItem} :
+    //Update feedback
+
+    const updateFeedback = async (id, updItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updItem)
+        })
+
+        const data = await response.json()
+
+        setFeedback(
+            feedback.map((item) => item.id === id? {
+            ...item, ...data} :
             item
             ))
     }
