6a3b65e031d0423702a52ef385c816c2fa29ff7f
GenoSamuel <edwin.grigonis2@mail.dcu.ie>
2022-04-28 11:05:13 +0900

    added server js

---
+++ server.js
@@ -0,0 +1,12 @@
+import data from "db.json";
+
+import { create, router as _router, defaults } from "json-server";
+const server = create();
+const router = _router(data);
+const middlewares = defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
